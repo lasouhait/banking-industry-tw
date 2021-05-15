336fc63484da03be700410312f130ab75887eec7
lasouhait <62388539+lasouhait@users.noreply.github.com>
2021-05-15 23:02:53 +0800

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -6,5 +6,12 @@ import pandas as pd
 st.title("臺灣金融業統計")
 st.write("Dashboard")
 
-df = pd.read_csv("大盤_2016.csv")
+df_2016 = pd.read_csv("大盤_2016.csv")
+df_2017 = pd.read_csv("大盤_2017.csv")
+df_2018 = pd.read_csv("大盤_2018.csv")
+df_2019 = pd.read_csv("大盤_2019.csv")
+df_2020 = pd.read_csv("大盤_2020.csv")
+df_2021 = pd.read_csv("大盤_2021.csv")
+
+
 st.write(df.iloc[0:1])
