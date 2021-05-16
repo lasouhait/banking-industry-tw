e8590cdb6e0810f30b96f7828ca1c6938d231fc2
lasouhait <62388539+lasouhait@users.noreply.github.com>
2021-05-16 17:31:57 +0800

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -33,17 +33,18 @@ Company = Selection_Name_Dict[st.selectbox("選擇證券名稱",list(Selection_L
 st.write(Company+" 歷年年間最高、最低股價")
 Stock_Annual_Summary = pd.DataFrame(columns=['證券代號','證券名稱','最低價','最低價日期','最高價','最高價日期','年初開盤價','年末收盤價'])
 
-S2016 = df_Bank_2016[df_Bank_2016["證券名稱"]==Company]
-Company_Num = S2016['證券代號'].unique()
-min_record = S2016[S2016["最低價"]==S2016["最低價"].min()]
-min_price = min_record['最低價']
-min_day = min_record['日期']
-max_record = S2016[S2016["最低價"]==S2016["最低價"].max()]
-max_price = max_record['最低價']
-max_day = max_record['日期']
-first_open = S2016[S2016["日期"]==S2016["日期"].min()]["開盤價"]
-last_close = S2016[S2016["日期"]==S2016["日期"].max()]["收盤價"]
+for i in [df_Bank_2016,df_Bank_2017,df_Bank_2018,df_Bank_2019,df_Bank_2020,df_Bank_2021]:
+  S = i[i["證券名稱"]==Company]
+  Company_Num = S['證券代號'].unique()
+  min_record = S[S["最低價"]==S["最低價"].min()]
+  min_price = min_record['最低價']
+  min_day = min_record['日期']
+  max_record = S[S["最低價"]==S["最低價"].max()]
+  max_price = max_record['最低價']
+  max_day = max_record['日期']
+  first_open = S[S["日期"]==S["日期"].min()]["開盤價"]
+  last_close = S[S["日期"]==S["日期"].max()]["收盤價"]
 
-Stock_Annual_Summary.append({'證券代號': Company_Num, '證券名稱': Company, '最低價': min_price, '最低價日期': min_day, '最高價': max_price, '最高價日期': max_day, '年初開盤價': first_open, '年末收盤價': last_close},ignore_index=True)
+  Stock_Annual_Summary.append({'證券代號': Company_Num, '證券名稱': Company, '最低價': min_price, '最低價日期': min_day, '最高價': max_price, '最高價日期': max_day, '年初開盤價': first_open, '年末收盤價': last_close},ignore_index=True,inplace=True)
 
 st.write(Stock_Annual_Summary)
