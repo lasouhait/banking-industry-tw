2c0731b4199f3ac543b4b5174db80407e7bab9d8
lasouhait <62388539+lasouhait@users.noreply.github.com>
2021-05-19 13:12:11 +0800

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -22,6 +22,12 @@ df_Bank_2019 = pd.read_csv("金融股_2019.csv")
 df_Bank_2020 = pd.read_csv("金融股_2020.csv")
 df_Bank_2021 = pd.read_csv("金融股_2021.csv")
 
+index = pd.DataFrame(columns=['指數','收盤指數','日期']
+                     
+for j in [df_2016,df_2017,df_2018,df_2019,df_2020,df_2021]:
+  j = j[j["指數"]=="發行量加權股價指數"]
+  index = index.append(j,ignore_index=True)                     
+
 Company_List = df_Bank_2016[["證券代號","證券名稱"]].append(df_Bank_2017[["證券代號","證券名稱"]]).append(df_Bank_2018[["證券代號","證券名稱"]]).append(df_Bank_2019[["證券代號","證券名稱"]]).append(df_Bank_2020[["證券代號","證券名稱"]]).append(df_Bank_2021[["證券代號","證券名稱"]])
 Company_List = Company_List.sort_values("證券代號")
 Company_List["清單"] = Company_List["證券代號"]+" "+Company_List["證券名稱"]
@@ -52,9 +58,8 @@ for i in [df_Bank_2016,df_Bank_2017,df_Bank_2018,df_Bank_2019,df_Bank_2020,df_Ba
     Stock_Annual_Summary = Stock_Annual_Summary.append({'年':Year, '證券代號': Company_Num, '證券名稱': Company, '最低價': min_price, '最低價日期': min_day, '最高價': max_price, '最高價日期': max_day, '年初開盤價': first_open, '年末收盤價': last_close},ignore_index=True)
 
     Stock_PCT = Stock_PCT.append(S[['年','證券代號','證券名稱','收盤價','日期']])
-    for j in [df_2016,df_2017,df_2018,df_2019,df_2020,df_2021]:
-      j = j[j["指數"]=="發行量加權股價指數"]
-      Stock_PCT = Stock_PCT.join(j.set_index('日期')["收盤指數"],on='日期')
+    Stock_PCT = Stock_PCT.join(index.set_index('日期'),on='日期')
+                     
   except:
     pass  
 Stock_Annual_Summary = Stock_Annual_Summary.set_index('年')
