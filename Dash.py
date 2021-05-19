5acd0b44e7f3ce0164aa8c9bab4af6a18cdb34da
lasouhait <62388539+lasouhait@users.noreply.github.com>
2021-05-19 22:37:42 +0800

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -7,6 +7,7 @@ import statsmodels.api as sm
 st.set_page_config(page_title="臺灣金融業資訊分析")
 st.title("臺灣金融業資訊分析")
 
+df_2015 = pd.read_csv("大盤_2015.csv")
 df_2016 = pd.read_csv("大盤_2016.csv")
 df_2017 = pd.read_csv("大盤_2017.csv")
 df_2018 = pd.read_csv("大盤_2018.csv")
@@ -14,6 +15,7 @@ df_2019 = pd.read_csv("大盤_2019.csv")
 df_2020 = pd.read_csv("大盤_2020.csv")
 df_2021 = pd.read_csv("大盤_2021.csv")
 
+df_Bank_2015 = pd.read_csv("金融股_2015.csv")
 df_Bank_2016 = pd.read_csv("金融股_2016.csv")
 df_Bank_2017 = pd.read_csv("金融股_2017.csv")
 df_Bank_2018 = pd.read_csv("金融股_2018.csv")
@@ -23,14 +25,14 @@ df_Bank_2021 = pd.read_csv("金融股_2021.csv")
 
 index = pd.DataFrame(columns=['收盤指數','日期'])
 
-for j in [df_2016,df_2017,df_2018,df_2019,df_2020,df_2021]:
+for j in [df_2015,df_2016,df_2017,df_2018,df_2019,df_2020,df_2021]:
     j = j[j["指數"]=="發行量加權股價指數"][['收盤指數','日期']]
     j['收盤指數'] = j['收盤指數'].str.replace(",","").str.replace("\"","").astype(float)
     index = index.append(j,ignore_index=True)
 
 index = index.set_index("日期")
    
-Company_List = df_Bank_2016[["證券代號","證券名稱"]].append(df_Bank_2017[["證券代號","證券名稱"]]).append(df_Bank_2018[["證券代號","證券名稱"]]).append(df_Bank_2019[["證券代號","證券名稱"]]).append(df_Bank_2020[["證券代號","證券名稱"]]).append(df_Bank_2021[["證券代號","證券名稱"]])
+Company_List = df_Bank_2015[["證券代號","證券名稱"]].append(df_Bank_2016[["證券代號","證券名稱"]]).append(df_Bank_2017[["證券代號","證券名稱"]]).append(df_Bank_2018[["證券代號","證券名稱"]]).append(df_Bank_2019[["證券代號","證券名稱"]]).append(df_Bank_2020[["證券代號","證券名稱"]]).append(df_Bank_2021[["證券代號","證券名稱"]])
 Company_List = Company_List.sort_values("證券代號")
 Company_List["清單"] = Company_List["證券代號"]+" "+Company_List["證券名稱"]
 Selection_List = Company_List["清單"].unique()
@@ -42,7 +44,7 @@ st.write(Company+" 歷年年間最高、最低股價")
 Stock_Annual_Summary = pd.DataFrame(columns=['年','證券代號','證券名稱','最低價','最低價日期','最高價','最高價日期','年初開盤價','年末收盤價'])
 Stock_PCT = pd.DataFrame(columns=['年','證券代號','證券名稱','收盤價','日期'])
 
-for i in [df_Bank_2016,df_Bank_2017,df_Bank_2018,df_Bank_2019,df_Bank_2020,df_Bank_2021]:
+for i in [df_Bank_2016,df_Bank_2017,df_Bank_2018,df_Bank_2019,df_Bank_2020,df_Bank_2021]:#未放入df_Bank_2015，因僅有最後一個交易日
   try:
     S = i[i["證券名稱"]==Company]
     Year = S['年'].unique()[0]
@@ -67,6 +69,7 @@ Stock_Annual_Summary = Stock_Annual_Summary.set_index('年')
 Stock_PCT['收盤價'] = Stock_PCT['收盤價'].astype(float)
 Stock_PCT["漲跌幅"] = Stock_PCT['收盤價'].pct_change()
 Stock_PCT["大盤波動幅"] = Stock_PCT['收盤指數'].pct_change()
+Stock_PCT = Stock_PCT[1:]
 st.write(Stock_Annual_Summary)
 
 st.write(Company+" 股價波動分析")
